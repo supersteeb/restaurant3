9a09ea7d7d8848b1084a093b231df12da02e9760
Steve Cho <supersteeb@gmail.com>
2016-10-13 15:41:54 +0700

    updated menu filter

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,8 +1,10 @@
 class MenuController < ApplicationController
   def index
   	@food_items = FoodItem.all
-  	if params[:section]
+  	if params[:section].present?
   		@section = Section.where(name: params[:section]).first
+
+  		@food_items = @section.food_items if @section.present?
   	end
   end
 end
