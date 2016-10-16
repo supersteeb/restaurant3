f9a0a73954d048b35ea5458f3f92a38a05ef120e
Steve Cho <supersteeb@gmail.com>
2016-10-16 21:51:33 +0700

    added 'optional: true' to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  belongs_to :food_item
+  belongs_to :food_item, optional: true
   has_many :order_food_items#, :food_items 
 
   validates :food_item, :name, presence: true
